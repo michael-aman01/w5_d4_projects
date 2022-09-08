36f00afac8205bdec1836bd434f6a146ac2561a6
Brendan <brendantsuda@gmail.com>
2022-09-08 15:13:53 -0700

    Finish enrollments, enrolled_students methods for Course

--- practice-for-ch-sql-associations-long-practice/app/models/course.rb
+++ practice-for-ch-sql-associations-long-practice/app/models/course.rb
@@ -11,4 +11,13 @@
 #
 class Course < ApplicationRecord
     
+    has_many :enrollments,
+        primary_key: :id,
+        foreign_key: :course_id,
+        class_name: :Enrollment
+
+    has_many :enrolled_students,
+        through: :enrollments,
+        source: :student
+        
 end
